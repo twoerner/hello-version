d8534d2ab33036610cd3d4ffe9118da42d2b3bf5
Trevor Woerner <twoerner@gmail.com>
2022-01-13 11:33:12 -0500

    initial commit: version 1.0.0
    Signed-off-by: Trevor Woerner <twoerner@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2022  Trevor Woerner <twoerner@gmail.com>
+ */
+
+#include <stdio.h>
+#include "config.h"
+
+int
+main (void)
+{
+	printf("hello, version %s\n", VERSION);
+	return 0;
+}
